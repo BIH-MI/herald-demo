fcae763c2befa5090d554ed75f71f3a38893401d
bihmi-lb <lena.baum@charite.de>
2024-04-07 13:38:44 +0200

    TTE vizualization: only plot div for data column if positive time-to-event values exist, so the plot is not empty

--- analytics-demo/ghdm-render-time-to-event.js
+++ analytics-demo/ghdm-render-time-to-event.js
@@ -52,11 +52,11 @@ function visualizeKaplanMeierCurves(cohortLabels, tables, indexEventColumnName, 
 	const colors = ['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink', 'gray', 'cyan', 'magenta'];
 
 	// Finde indices of all columns that are not id, age, sex or index
-	const outcomeOventColumns = [];
+	const outcomeEventColumns = [];
 	for (let i =0; i < tables[0][0].length; i++) {
 		let columnName = tables[0][0][i];
 		if (columnName !== "Patient ID" && columnName !== "Age" && columnName !== "Sex" && columnName !== indexEventColumnName) {
-			outcomeOventColumns.push(i);
+			outcomeEventColumns.push(i);
 		}
 	}
 
@@ -64,122 +64,135 @@ function visualizeKaplanMeierCurves(cohortLabels, tables, indexEventColumnName, 
 	const indexEventColumn = tables[0][0].indexOf(indexEventColumnName);
 
 	// For each such column
-	outcomeOventColumns.forEach((outcomeEventColumn) => {
+	outcomeEventColumns.forEach((outcomeEventColumn) => {
 
 		// Get column name
 		let columnName = tables[0][0][outcomeEventColumn];
 
-		// Create a separate div
-		const cohortDiv = document.createElement('div');
-		cohortDiv.id = `column-${outcomeEventColumn}`;
-		cohortDiv.style.width = '100%';
-		document.getElementById(outputDivId).appendChild(cohortDiv);
-
-		// Create a heading
-		const columnHeading = document.createElement('h3');
-		columnHeading.textContent = columnName;
-		cohortDiv.appendChild(columnHeading);
-
-		// Collect series for each cohort
-		const series = [];
-		let maxDays = 0;
+		//Log if column has data to display (i.e. positive time differences exist that can be visualized)
+		let hasPositiveTimeDifference = false;
+		// Process each cohort
 		for (let i = 0; i < cohortLabels.length; i++) {
-			let cohortSeries = calculateTimeToEvent(tables[i], indexEventColumn, outcomeEventColumn);
-			if (cohortSeries !== undefined && cohortSeries.length > 0) {
-				for (let days of cohortSeries) {
-					if (days > maxDays) {
-						maxDays = days;
+			let cohortSeries = calculateTimeToEvent(tables[i], indexEventColumn, outcomeEventColumn, false);
+			if (cohortSeries.some(time => time > 0)) {
+				hasPositiveTimeDifference = true;
+			}
+		}
+	
+		// Only plot if positive time differences are found
+		if(hasPositiveTimeDifference){
+			// Create a separate div
+			const cohortDiv = document.createElement('div');
+			cohortDiv.id = `column-${outcomeEventColumn}`;
+			cohortDiv.style.width = '100%';
+			document.getElementById(outputDivId).appendChild(cohortDiv);
+
+			// Create a heading
+			const columnHeading = document.createElement('h3');
+			columnHeading.textContent = columnName;
+			cohortDiv.appendChild(columnHeading);
+
+			// Collect series for each cohort
+			const series = [];
+			let maxDays = 0;
+			for (let i = 0; i < cohortLabels.length; i++) {
+				let cohortSeries = calculateTimeToEvent(tables[i], indexEventColumn, outcomeEventColumn);
+				if (cohortSeries !== undefined && cohortSeries.length > 0) {
+					for (let days of cohortSeries) {
+						if (days > maxDays) {
+							maxDays = days;
+						}
 					}
+					series.push(cohortSeries);
 				}
-				series.push(cohortSeries);
 			}
-		}
 
-		// Find best time unit to use
-		let timeUnit;
-		let timeFactor;
-		// One month
-		if (maxDays <= 30) {
-			timeUnit = 'Days';
-			timeFactor = 1;
-		// Five years
-		} else if (maxDays <= 1460) {
-			timeUnit = 'Months';
-			timeFactor = 30;
-		// More than two years
-		} else {
-			timeUnit = 'Years';
-			timeFactor = 365;
-		}
+			// Find best time unit to use
+			let timeUnit;
+			let timeFactor;
+			// One month
+			if (maxDays <= 30) {
+				timeUnit = 'Days';
+				timeFactor = 1;
+			// Five years
+			} else if (maxDays <= 1460) {
+				timeUnit = 'Months';
+				timeFactor = 30;
+			// More than two years
+			} else {
+				timeUnit = 'Years';
+				timeFactor = 365;
+			}
 
-		// Wrap into traces
-		// TODO: This does not handle censoring
-		const traces = [];
-		for (let i = 0; i < cohortLabels.length; i++) {
+			// Wrap into traces
+			// TODO: This does not handle censoring
+			const traces = [];
+			for (let i = 0; i < cohortLabels.length; i++) {
 
-			// Calculate time to event from index to outcome event
-			const plotData = calculateTimeToEvent(tables[i], indexEventColumn, outcomeEventColumn);
-			
-			// If we have some data
-			if (plotData !== undefined && plotData.length > 0) {
+				// Calculate time to event from index to outcome event
+				const plotData = calculateTimeToEvent(tables[i], indexEventColumn, outcomeEventColumn);
+				
+				// If we have some data
+				if (plotData !== undefined && plotData.length > 0) {
 
-				// Sort
-				const sortedData = plotData.sort((a, b) => a - b);
+					// Sort
+					const sortedData = plotData.sort((a, b) => a - b);
 
-				// Build trace. Start at timepoint 0, all individuals are survivors
-				const x = [0];
-				const y = [1];
+					// Build trace. Start at timepoint 0, all individuals are survivors
+					const x = [0];
+					const y = [1];
 
-				// For each timepoint
-				for(let i = 0; i < sortedData.length; i++) {
-					// Timepoint
-					x.push(sortedData[i] / timeFactor);
-					// Number of survivors at this timepoint
-					y.push(1 - (i / sortedData.length));
+					// For each timepoint
+					for(let i = 0; i < sortedData.length; i++) {
+						// Timepoint
+						x.push(sortedData[i] / timeFactor);
+						// Number of survivors at this timepoint
+						y.push(1 - (i / sortedData.length));
+					}
+
+					// Build trace
+					const trace = {
+						x: x,
+						y: y,
+						mode: 'lines+markers',
+						name: cohortLabels[i],
+						line: { color: colors[i % colors.length], shape: 'hv' },
+						marker: { size: 0 }
+					};
+
+					// Push trace
+					traces.push(trace);
 				}
-
-				// Build trace
-				const trace = {
-					x: x,
-					y: y,
-					mode: 'lines+markers',
-					name: cohortLabels[i],
-					line: { color: colors[i % colors.length], shape: 'hv' },
-					marker: { size: 0 }
-				};
-
-				// Push trace
-				traces.push(trace);
 			}
+
+			// Create div
+			const plotDiv = document.createElement('div');
+			plotDiv.style.width = '100%';
+			cohortDiv.appendChild(plotDiv);
+
+			// Specify layout
+			const layout = {
+				title: `Kaplan-Meier Curve for ${columnName}`,
+				xaxis: { title: timeUnit },
+				yaxis: { title: 'Fraction without event' },
+				margin: { l: 50, r: 50, b: 50, t: 50 },
+				height: 400,
+				autosize: true,
+				legend: {
+					x: 0.5,
+					y: 1.1,
+					xanchor: 'center',
+					yanchor: 'top',
+					orientation: 'h'
+				}
+			};
+
+			// Config
+			const config = { responsive: true };
+
+			// Create plot
+			Plotly.newPlot(plotDiv, traces, layout, config);
 		}
-
-		// Create div
-		const plotDiv = document.createElement('div');
-		plotDiv.style.width = '100%';
-		cohortDiv.appendChild(plotDiv);
-
-		// Specify layout
-		const layout = {
-			title: `Kaplan-Meier Curve for ${columnName}`,
-			xaxis: { title: timeUnit },
-			yaxis: { title: 'Fraction without event' },
-			margin: { l: 50, r: 50, b: 50, t: 50 },
-			height: 400,
-			autosize: true,
-			legend: {
-				x: 0.5,
-				y: 1.1,
-				xanchor: 'center',
-				yanchor: 'top',
-				orientation: 'h'
-			}
-		};
-
-		// Config
-		const config = { responsive: true };
-
-		// Create plot
-		Plotly.newPlot(plotDiv, traces, layout, config);
 	});
 }
 
